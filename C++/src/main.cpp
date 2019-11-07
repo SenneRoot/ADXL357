004eb7e5eee1f01e6a196fccdba545b7fe40d18d
SenneRoot <senner@hotmail.nl>
2019-11-07 15:25:39 +0100

    test

--- C++/src/main.cpp
+++ C++/src/main.cpp
@@ -1,31 +1,62 @@
 #include <iostream>
-#include <fstream>
 #include <string>
-#include <vector>
+#include "sockpp/tcp_connector.h"
+#include "sockpp/version.h"
 
-#include <sys/stat.h>
+using namespace std;
+using namespace std::chrono;
 
-int main(int argc, char *argv[]) {
-    const char *fifo_name = "fifo";
-    mknod(fifo_name, S_IFIFO | 0666, 0);
-    std::ifstream f(fifo_name);
-    std::string line;
-    getline(f, line);
-    auto data_size = std::stoi(line);
-    std::cout << "Size: " << data_size << std::endl;
-    std::string data;
-    {
-        std::vector<char> buf(data_size);
-        f.read(buf.data(), data_size);
-        // write to vector data is valid since C++11
-        data.assign(buf.data(), buf.size());
+int main(int argc, char* argv[])
+{
+	cout << "Sample TCP echo client for 'sockpp' "
+		<< sockpp::SOCKPP_VERSION << '\n' << endl;
+
+	string host = (argc > 1) ? argv[1] : "localhost";
+	in_port_t port = (argc > 2) ? atoi(argv[2]) : 12345;
+
+	sockpp::socket_initializer sockInit;
+
+	// Implicitly creates an inet_address from {host,port}
+	// and then tries the connection.
+
+	sockpp::tcp_connector conn({host, port});
+	if (!conn) {
+		cerr << "Error connecting to server at "
+			<< sockpp::inet_address(host, port)
+			<< "\n\t" << conn.last_error_str() << endl;
+		return 1;
+	}
+
+	cout << "Created a connection from " << conn.address() << endl;
+
+    // Set a timeout for the responses
+    if (!conn.read_timeout(seconds(5))) {
+        cerr << "Error setting timeout on TCP stream: "
+                << conn.last_error_str() << endl;
     }
-    if (!f.good()) {
-        std::cerr << "Read failed" << std::endl;
-    }
-    std::cout << "Data size: " << data.size() << " content: " << data << std::endl;
+
+	string s, sret;
+	while (getline(cin, s) && !s.empty()) {
+		if (conn.write(s) != ssize_t(s.length())) {
+			cerr << "Error writing to the TCP stream: "
+				<< conn.last_error_str() << endl;
+			break;
+		}
+
+		sret.resize(s.length());
+		ssize_t n = conn.read_n(&sret[0], s.length());
+
+		if (n != ssize_t(s.length())) {
+			cerr << "Error reading from TCP stream: "
+				<< conn.last_error_str() << endl;
+			break;
+		}
+
+		cout << sret << endl;
+	}
+
+	return (!conn) ? 1 : 0;
 }
-
 
 /*#include <iostream>
 #include "ADXL357.hpp"
