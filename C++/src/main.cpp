dc3f5a6a04ab3d2d20202b4b131bbae6f54f9572
SenneRoot <senner@hotmail.nl>
2019-11-08 10:03:07 +0100

    added \n to sending

--- C++/src/main.cpp
+++ C++/src/main.cpp
@@ -38,7 +38,7 @@ int main()
 		if(!message.compare("log"))
 		{
 			log = true;
-			udpsocket.send("logging data", &clientAddress, &len);
+			udpsocket.send("logging data\n", &clientAddress, &len);
 		}
 		else
 			log = false;
