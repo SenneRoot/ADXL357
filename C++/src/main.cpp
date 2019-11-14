673de00c04f6dde36d3912e449a397cd52cf4a28
SenneRoot <senner@hotmail.nl>
2019-11-14 09:58:09 +0100

    just work

--- C++/src/main.cpp
+++ C++/src/main.cpp
@@ -12,7 +12,7 @@
 int main()
 {
 	udpSocket udpsocket(8080);
-	vector<Sample> samples;
+	std::vector<Sample> samples;
 	ADXL357 adxl357;
 	bool writeData = true;
 	bool log = false;
@@ -51,7 +51,7 @@ int main()
 			Logger logger(&adxl357);
 			clock_t begin = clock();
 			cout << "Starting Logging dat for " << time << " seconds to gather " << 4000 * time << " samples" << endl;
-			std::thread logger_thread(&Logger::log, logger, std::ref(samples) ,time, true);
+			std::thread logger_thread(&Logger::log, logger, std::ref(samples));
 			//logger.log(&samples, time, true);
 			clock_t end = clock();
 			double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
