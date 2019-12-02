8e1deb3d920cd1d57c258fdecd1451c90e3f815b
SenneRoot <senner@hotmail.nl>
2019-12-02 13:12:28 +0100

    new main

--- C++/src/main.cpp
+++ C++/src/main.cpp
@@ -16,8 +16,8 @@ int main()
 	//tcpSocket tcpsocket(8081);
 	vector<Sample> samples;
 	ADXL357 adxl357;
-	bool writeData = true;
-	bool log = true;
+	//bool writeData = true;
+	bool log = false;
 	int time = 4;
 	string dataPath = "../data/data_test.csv";
 
@@ -29,65 +29,19 @@ int main()
 
 	while (1)
 	{
-		char buf[MAXLINE];
-		struct sockaddr clientAddress;
-		uint len;
-
-		udpsocket.receive(buf, &clientAddress, &len);
-		std::string message(buf);
-		//printf("%s", buf);
-		message.pop_back();
-		cout << message << endl;
-
+		//wait for message from python parent proc to start logging
+		char* buf;
+		fread(buf, 1, 3, stdin);
+		std::string message(*buf)
 		if(!message.compare("log"))
 		{
 			log = true;
-			//udpsocket.send("logging data\n", &clientAddress, &len);
 		}
-		else
-			log = false;
 
 		if (log)
 		{
 			Logger logger(&adxl357);
-			//clock_t begin = clock();
-			//cout << "Starting Logging dat for " << time << " seconds to gather " << 4000 * time << " samples" << endl;
-			//std::thread logger_thread(&Logger::logContinuousTCP, logger, std::ref(tcpsocket));
-			logger.logContinuousUDP(&udpsocket, &clientAddress, &len);
-			//clock_t end = clock();
-			//double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
-			//cout << "Resulting log: " << endl;
-			//cout << "Number of samples: " << samples.size() << " Time: " << elapsed_secs << endl;
-		}
-
-		if (writeData)
-		{
-			/*ofstream out(dataPath);
-			if (!out)
-			{
-				cout << "Couldn't open file." << endl;
-				return 1;
-			}
-
-			out << "x" << endl;
-					//<< ","
-					//<< "y"
-					//<< ","
-					//<< "z" << endl;*/
-
-			/*for (auto &sample : samples)
-			{
-				//out << sample.getX() << "," << sample.getY() << "," << sample.getZ() << endl;
-				//out << sample.getX() << endl;
-				double X = sample.getX();
-				double Y = sample.getY();
-				double Z = sample.getZ();
-				udpsocket.send(&X, &clientAddress, &len);
-				udpsocket.send(&Y, &clientAddress, &len);
-				udpsocket.send(&Z, &clientAddress, &len);
-			}
-
-			udpsocket.send("done", &clientAddress, &len);*/
+			logger.log(&samples, time);
 		}
 	}
 }
