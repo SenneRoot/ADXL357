8e38a0142d559d93200775ac941fe7b7c76205a5
SenneRoot <senner@hotmail.nl>
2019-12-03 11:29:22 +0100

    testing button

--- C++/src/main.cpp
+++ C++/src/main.cpp
@@ -3,12 +3,23 @@
 #include "Logger.hpp"
 #include "Sample.hpp"
 #include "stdio.h"
-##include <mqtt/client.h>
+//#include <mqtt/client.h>
 #include <wiringPi.h>
+
+#define btn_pin 8
+
+void setupGPIO()
+{
+		wiringPiSetup();
+
+		pinMode(btn_pin, INPUT);
+		pullUpDnControl(btn_pin, PUD_UP);
+}
+
 
 int main()
 {
-	wiringPiSetup();
+	setupGPIO();
 	vector<Sample> samples;
 	ADXL357 adxl357;
 	bool log = false;
@@ -22,6 +33,11 @@ int main()
 
 	while (1)
 	{
-
+		Logger logger(&adxl357);
+		if(!digitalRead(btn_pin))
+		{
+			printf("logging samples");
+			logger.log(samples, time, true);
+		}
 	}
 }
