bea58fcf4f68b5410a47e92fc3b584a292d36ab3
SenneRoot <senner@hotmail.nl>
2019-12-10 11:39:28 +0100

    json format test

--- C++/src/main.cpp
+++ C++/src/main.cpp
@@ -59,28 +59,6 @@ int main(int argc, char* argv[])
 	adxl357.dumpInfo();
 	double rate = adxl357.getRate();
 
-	//setup MQTT
-	/*string address = (argc > 1) ? string(argv[1]) : DFLT_ADDRESS;
-	mqtt::async_client cli(address, "", MAX_BUFFERED_MSGS, PERSIST_DIR);
-	mqtt::connect_options connOpts;
-	connOpts.set_keep_alive_interval(MAX_BUFFERED_MSGS * PERIOD);
-	connOpts.set_clean_session(true);
-	connOpts.set_automatic_reconnect(true);
-	// Create a topic object. This is a conventience since we will
-	// repeatedly publish messages with the same parameters.
-	mqtt::topic top(cli, TOPIC, QOS, true);
-
-	try {
-		// Connect to the MQTT broker
-		cout << "Connecting to server '" << address << "'..." << flush;
-		cli.connect(connOpts)->wait();
-		cout << "OK\n" << endl;
-	}
-	catch (const mqtt::exception& exc) {
-		cerr << exc.what() << endl;
-		return 1;
-	}*/
-
 	while (1)
 	{
 		Logger logger(&adxl357);
@@ -96,12 +74,10 @@ int main(int argc, char* argv[])
 			//log Continuous, the time parameter determines the polling interval
 			while(!digitalRead(btn_pin))
 			{
-				//logger.log(samples, time, true, true);
 				logger.logContinuous(samples, rate, time, true);
 				printf("\rLogging ---> %6d", samples.size());
         fflush(stdout);
 			}
-
 			//its now safe to put the sensor back in measurement mode
 			adxl357.stop();
 			logged = true;
@@ -110,12 +86,19 @@ int main(int argc, char* argv[])
 	 	//send the logged samples over MQTT protocol
 		if(logged)
 		{
-			string payload = to_string(samples.size()) + " logged at " + tmbuf + "\n";
+			string date = "\"" + tmbuf + "\"";
+			string sensor = "\"ADXL357\"";
+			string nSamples = "\"" + to_string(samples.size()) + "\"";
+			string samples = "["
+
+
 			for (auto& sample : samples)
 			{
-				payload += to_string(sample.getX()) + "," + to_string(sample.getY()) + "," + to_string(sample.getZ()) + "\n";
+				samples += "\"" + to_string(sample.getX()) + "\"" + "," + "\"" + to_string(sample.getY()) + "\""  + "," + "\"" + to_string(sample.getZ()) + "\"" + ",";
 			}
 
+			samples += "]";
+			string payload = "\"Sensor\" : " + sensor + "\"Time\" : " + date + "\"NumberSamples\" : " + nSamples + "\"Samples\" : " + samples;
 			//Publish to the topic
 			//top.publish(std::move(payload));
 			sender.send(payload, "testTopic");
@@ -128,82 +111,3 @@ int main(int argc, char* argv[])
 		// Disconnect
 		return 0;
 }
-
-
-
-
-
-
-
-//#include <iostream>
-/*#include "ADXL357.hpp"
-#include "Logger.hpp"
-#include "Sample.hpp"
-#include "stdio.h"
-//#include <mqtt/client.h>
-#include <wiringPi.h>
-#include <unistd.h>
-
-#define btn_pin 8
-
-void setupGPIO()
-{
-		wiringPiSetup();
-
-		pinMode(btn_pin, INPUT);
-		pullUpDnControl(btn_pin, PUD_UP);
-}
-
-bool read_btn(int btnPin)
-{
-	//debounce pin
-	if(!digitalRead(btnPin) || digitalRead(btnPin))
-	{
-		usleep(1);
-		return digitalRead(btnPin);
-	}
-	//should never reach this part
-	return true;
-}
-
-
-int main()
-{
-	setupGPIO();
-	vector<vector<Sample>> samples;
-	ADXL357 adxl357;
-	bool logged = false;
-	double time = 0.005;
-
-	//setup ADXL357 sensor
-	adxl357.stop();
-	adxl357.setRange(SET_RANGE_10G);
-	adxl357.setFilter(SET_HPF_OFF, SET_ODR_4000);
-	adxl357.dumpInfo();
-
-	while (1)
-	{
-		Logger logger(&adxl357);
-
-		if(!read_btn(btn_pin))
-		{
-			//samples.clear();
-			vector<Sample> temp;
-			while(!digitalRead(btn_pin))
-			{
-				logger.log(temp, time, true, true);
-				printf("\rLogging ---> %6d", temp.size());
-        fflush(stdout);
-			}
-			samples.push_back(temp);
-			logged = true;
-		}
-
-		if(logged)
-		{
-			printf("\nDone! logged %2d times\n", samples.size());
-			logged = false;
-		}
-	}
-}*/
-
