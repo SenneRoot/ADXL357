89cd47e3acde8f43a23222fdfc8450d3f519bfbf
SenneRoot <senner@hotmail.nl>
2019-12-10 10:20:56 +0100

    fixes

--- C++/src/Sender.cpp
+++ C++/src/Sender.cpp
@@ -1,15 +1,15 @@
 #include "Sender.hpp"
 
-Sender::Sender(std::string address, std::string client_id , int qos, std::string persist_dir = "data-persist")
+Sender::Sender(std::string address, std::string client_id , int qos, std::string persist_dir)
 {
 	m_address = address;
 	m_qos = qos;
 	m_persist_dir = persist_dir;
 	m_client_id = client_id;
 
-	cli = new mqtt::async_client(m_address, client_id, MAX_BUFFERED_MSGS, m_persist_dir);
+	m_cli = new mqtt::async_client(m_address, client_id, MAX_BUFFERED_MSGS, m_persist_dir);
 
-	m_conn_opt.set_keep_alive_interval(MAX_BUFFERED_MSGS * PERIOD);
+	m_conn_opt.set_keep_alive_interval(MAX_BUFFERED_MSGS);
 	m_conn_opt.set_clean_session(true);
 	m_conn_opt.set_automatic_reconnect(true);
 
@@ -21,7 +21,6 @@ Sender::Sender(std::string address, std::string client_id , int qos, std::string
 	}
 	catch (const mqtt::exception& exc) {
 		cerr << exc.what() << endl;
-		return 1;
 	}
 }
 
