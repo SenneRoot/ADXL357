a649117e1c82fa7ac8a6da23396a48bdd20b5b82
Senne Root <senner@hotmail.nl>
2019-12-12 11:01:55 +0000

    removed debug print

--- C++/src/Sender.cpp
+++ C++/src/Sender.cpp
@@ -16,7 +16,7 @@ Sender::Sender(std::string address, std::string client_id, int qos, int mqtt_ver
 
 	try {
 		// Connect to the MQTT broker
-		cout << "MQTT version: " << m_conn_opt.get_mqtt_version() << endl;
+		//cout << "MQTT version: " << m_conn_opt.get_mqtt_version() << endl;
 		cout << "Connecting to server '" << address << "'..." << flush;
 		mqtt::token_ptr tok = m_cli->connect(m_conn_opt);
 		auto connRsp = tok->get_connect_response();
