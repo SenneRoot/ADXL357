e833231b6ccbdd8d05c86ade3f626dea0f166cdf
SenneRoot <senner@hotmail.nl>
2020-01-16 08:30:32 +0100

    constructor must be public

--- C++/include/Accel.hpp
+++ C++/include/Accel.hpp
@@ -3,10 +3,11 @@
 
 class Accel
 {
+public:
 	Accel(uint8_t channel, int speed, int mode, uint8_t bitsperword);
 	~Accel();
 
-public:
+
 	bool read(uint8_t reg, uint8_t *buf, size_t length = 1);
 	bool write(uint8_t reg, uint8_t val);
 
